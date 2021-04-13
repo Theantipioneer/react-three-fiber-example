b3d2bf9bdcce3e84d5d98acc233f11af62532a5b
theantipioneer <atangmotloli@gmail.com>
2021-04-13 09:57:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef} from 'react';
+import './App.scss';
+import {Canvas, useFrame} from 'react-three-fiber';
+
+// We are taking props here so we can use them in the App function as thats where the SpinningMesh function is going. 
+const SpinningMesh = ({position, args, color}) => {
+    const mesh = useRef(null);
+
+    useFrame(() => {
+    mesh.current.rotation.x = mesh.current.rotation.y += 0.01});
+
+  return(
+    <mesh position={position} ref={mesh}>
+      <boxBufferGeometry attach='geometry' args={args} />
+      <meshStandardMaterial attach='material' color={color} />
+    </mesh>
+  );
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas colorManagement>
+      <ambientLight intensity= {1} />
+      <pointLight position={[-10, 0, -20]} intensity={0.5} />
+      <pointLight position={[0, -10, 0]} intensity={1.5} />
+      <SpinningMesh position={[-1, 1, 0]} color='lightblue' args={[3,2,1]} />
+      <SpinningMesh position={[1.5,0.5, -0.5]} color='cyan'/>
+      <SpinningMesh position={[-0.5, -1.6, 0.2]} color='lime'/>
+
+    </Canvas>
+    
   );
 }
 
